51161ad8253b2c8ae15d9f39b694ba1f70164930
andresGarcia90 <garcia.andi90@gmail.com>
2022-11-06 11:10:31 -0300

    Create UI Button

--- src/Users/AddUsers.js
+++ src/Users/AddUsers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import "./AddUsers.css";
 
@@ -18,14 +19,13 @@ const AddUsers = (props) => {
 
   return (
     <Card>
-      <form onSubmit={handleSubmit}>
+      <form>
         <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
 
         <label htmlFor="age">Age (Year)</label>
         <input id="age" type="number"></input>
-
-        <button type="submit">Add user</button>
+        <Button type='submit' onClick={handleSubmit}>Add User</Button>
       </form>
     </Card>
   );
