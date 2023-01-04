6d910015a1e900c8e6ea4968b6fe0bba35e3500b
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-03 22:45:46 -0300

    Handle modal events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
-
+  const [cartIsShow, setCartIsShow] = useState(false);
+  const cartShowHandle = () => {setCartIsShow(true)};
+  const cartHideHandle = () => setCartIsShow(false);
   return (
     <>
-      <Cart></Cart>
-      <Header></Header>
-      <div>
+      {cartIsShow && <Cart onClose={cartHideHandle}/>}
+      <Header onShowCart={cartShowHandle}></Header>
+      <div>/
         <Meals></Meals>
       </div>
     </>
