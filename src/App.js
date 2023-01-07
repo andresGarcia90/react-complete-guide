c71aae8c38257bb8e3f40ebe02cf005fad09b47d
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-07 16:39:08 -0300

    Init Project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React, { useState } from 'react';
-import Cart from './components/Cart/Cart';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
-
 function App() {
-  const [cartIsShow, setCartIsShow] = useState(false);
-  const cartShowHandle = () => {setCartIsShow(true)};
-  const cartHideHandle = () => setCartIsShow(false);
   return (
-    <CartProvider>
-      {cartIsShow && <Cart onClose={cartHideHandle}/>}
-      <Header onShowCart={cartShowHandle}></Header>
-      <div>/
-        <Meals></Meals>
-      </div>
-    </CartProvider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
