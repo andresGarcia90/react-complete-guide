e93aed71dcde663f986b57470d17a6b1a3a929d7
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-09 16:31:50 -0300

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NewQuote from './pages/NewQuote';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <AllQuotes />,
+    },
+    {
+      path: '/new-quote',
+      element: <NewQuote />,
+    },
+    {
+      path: '/quotes/:quoteId',
+      element: <QuoteDetail />,
+    },
+    {
+      path: '*',
+      element: <AllQuotes />,
+    },
+  ]);
   return (
     <>
+      <RouterProvider router={router} />
     </>
   );
 }
