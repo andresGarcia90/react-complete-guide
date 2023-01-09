2df772c0fe78536486eb3801aafdf7556a6caf7e
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-09 18:11:13 -0300

    Adding Layout and main navigation

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great Quotes</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink
+              to={'/quotes'}
+              style={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              All Quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={'/new-quote'}
+              style={({ isActive }) => (isActive ? 'active' : '')}
+            >
+              Add Quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
