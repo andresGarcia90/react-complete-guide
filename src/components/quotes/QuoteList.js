11dd35ca76078dec625dd114f3ee598d2dddb5c8
andresGarcia90 <garcia.andi90@gmail.com>
2023-01-30 12:59:05 -0300

    Adding query params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,39 @@
 import { Fragment } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const [params, setParams] = useSearchParams();
+  const sortAscending = params.get('sort') === 'asc';
+  console.log(sortAscending);
+
+  const changeSortingHandler = () => {
+    params.delete('sort');
+    const sortDescription = !sortAscending ? 'asc' : 'dsc';
+    setParams({
+      sort: sortDescription,
+    });
+  };
+
+  const quotes = sortAscending ? props.quotes : props.quotes.sort((el1, el2) => {
+    if (el1.text > el2.text) {
+      return 1;
+    } else {
+      if (el1.text < el2.text) {
+        return -1;
+      } else return 0;
+    }
+  });
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort {sortAscending ? 'Ascending' : 'Descending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {quotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,9 +2,9 @@ import React from 'react';
 import QuoteList from '../components/quotes/QuoteList';
 
 const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Person 1', text: 'Text 1' },
-  { id: 'q2', author: 'Person 2', text: 'Text 2' },
-  { id: 'q3', author: 'Person 3', text: 'Text 3' },
+  { id: 'q1', author: 'Person 1', text: 'Zebra' },
+  { id: 'q2', author: 'Person 2', text: 'Spider' },
+  { id: 'q3', author: 'Person 3', text: 'Cobra' },
 ];
 function AllQuotes() {
 
